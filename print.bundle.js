79b249c6dd5bc781dae1a186f25891dd7c5ac7ee
Hank Liu <hank33liu@gmail.com>
2022-12-10 17:58:41 -0800

    Completes skeleton of html

--- print.bundle.js
+++ print.bundle.js
@@ -1,1 +1,19 @@
-console.log("Print is working!");
+(self["webpackChunkodin_restaurant_page"] = self["webpackChunkodin_restaurant_page"] || []).push([["print"],{
+
+/***/ "./src/print.js":
+/*!**********************!*\
+  !*** ./src/print.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log('print.js is working!')
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/print.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJpbnQuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9vZGluLXJlc3RhdXJhbnQtcGFnZS8uL3NyYy9wcmludC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygncHJpbnQuanMgaXMgd29ya2luZyEnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
